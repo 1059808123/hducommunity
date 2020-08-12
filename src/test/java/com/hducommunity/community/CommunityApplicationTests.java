21227640b561a1811533fc2e065f69753ab8f37f
1059808123 <1059808123@qq.com>
2020-08-12 10:45:36 +0800

    first commit

---
+++ src/test/java/com/hducommunity/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hducommunity.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
